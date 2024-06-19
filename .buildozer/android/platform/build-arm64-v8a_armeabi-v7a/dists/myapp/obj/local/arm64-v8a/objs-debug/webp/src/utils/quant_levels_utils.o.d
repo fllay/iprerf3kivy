da45d7cdb486f85af57e8a9b43ecb628fbb55bc7
fllay <calgalli@gmail.com>
2024-06-19 18:34:19 +0700

    add git ignore 2

--- .buildozer/android/platform/build-arm64-v8a_armeabi-v7a/dists/myapp/obj/local/arm64-v8a/objs-debug/webp/src/utils/quant_levels_utils.o.d
+++
@@ -1,8 +0,0 @@
-/Users/cake/Desktop/Projects/python/kivy/iperf3_app/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/arm64-v8a/objs-debug/webp/src/utils/quant_levels_utils.o: \
-  /Users/cake/Desktop/Projects/python/kivy/iperf3_app/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/utils/quant_levels_utils.c \
-  /Users/cake/Desktop/Projects/python/kivy/iperf3_app/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/utils/quant_levels_utils.h \
-  /Users/cake/Desktop/Projects/python/kivy/iperf3_app/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/types.h
-
-/Users/cake/Desktop/Projects/python/kivy/iperf3_app/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/utils/quant_levels_utils.h:
-
-/Users/cake/Desktop/Projects/python/kivy/iperf3_app/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/types.h:
--- .buildozer/android/platform/build-arm64-v8a_armeabi-v7a/dists/myapp/obj/local/armeabi-v7a/objs-debug/SDL2_image/src/IMG_WIC.o.d
+++
@@ -1,2 +0,0 @@
-/Users/cake/Desktop/Projects/python/kivy/iperf3_app/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/SDL2_image/src/IMG_WIC.o: \
-  /Users/cake/Desktop/Projects/python/kivy/iperf3_app/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/src/IMG_WIC.c
